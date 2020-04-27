47389af59f87fbd8e1fc64df1017267af7fdb229
Janis Lebron <janislebron@gmail.com>
2020-04-27 02:47:03 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
-def position_taken?(board, character)
-  if board[index_number] == "X" || "O"
-  else
-    board[index_number] == " " || "" || nil
-  end
+def position_taken?(board, index)
+if board[index] == " " || board[index] == "" || board[index] == nil
+  return false
+else
+  return true
+end
 end
